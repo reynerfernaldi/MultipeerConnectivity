c6360237dc5304c56cbf07c45b10674035e16462
Reyner Fernaldi <reynerfernaldi@gmail.com>
2023-08-09 09:47:02 +0700

    Initial Commit

---
+++ CobaMultiPeer/CobaMultiPeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobaMultiPeerApp.swift
+//  CobaMultiPeer
+//
+//  Created by Reyner Fernaldi on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CobaMultiPeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
